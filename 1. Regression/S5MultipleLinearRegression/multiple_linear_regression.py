aa15b01eea2c93876871dc3e43ade4268f5e4605
Brain Tucker <brian.briantucker@gmail.com>
2018-01-05 15:21:07 -0600

    started backward elimation by adding column of 1's to beginning of X matrix

--- 1. Regression/S5MultipleLinearRegression/multiple_linear_regression.py
+++ 1. Regression/S5MultipleLinearRegression/multiple_linear_regression.py
@@ -37,3 +37,6 @@ regressor.fit(X_train, y_train)
 # predicting the test set results
 y_pred = regressor.predict(X_test)
 
+# building the optimal model using Backward Elimination
+import statsmodels.formula.api as sm
+X = np.append(arr = np.ones((50,1)).astype(int), values = X, axis = 1)
