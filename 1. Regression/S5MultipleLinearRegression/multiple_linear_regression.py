a507831ad4af53e5e619bfd1450eccc0ad97d07a
Brain Tucker <brian.briantucker@gmail.com>
2018-01-05 14:58:23 -0600

    predicting test result and storing in y_pred

--- 1. Regression/S5MultipleLinearRegression/multiple_linear_regression.py
+++ 1. Regression/S5MultipleLinearRegression/multiple_linear_regression.py
@@ -1,17 +1,20 @@
+# Author @Brian Tucker
+# Jan 2018
+
 # Multiple Linear Regression
 
-# Importing the libraries
+# importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
+# importing the dataset
 dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 4].values
 
-# Encoding categorical data
-# Encoding the Independent Variable
+# encoding categorical data
+# encoding the Independent Variable
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
@@ -21,12 +24,16 @@ X = onehotencoder.fit_transform(X).toarray()
 # avoiding the dummy variable trap
 X = X[:, 1:]
 
-# Splitting the dataset into the Training set and Test set
+# splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 
-# Fitting to training set
+# fitting to training set
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
+
+# predicting the test set results
+y_pred = regressor.predict(X_test)
+
