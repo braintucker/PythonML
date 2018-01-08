bc0d560bdb217adce28b5b15a242e71d9958e9ec
Brain Tucker <brian.briantucker@gmail.com>
2018-01-08 10:05:50 -0600

    transforming the y_pred because the original data being used was transformed

--- 1. Regression/S7SupportVectorRegression(SVR)/svr.py
+++ 1. Regression/S7SupportVectorRegression(SVR)/svr.py
@@ -30,7 +30,7 @@ regressor.fit(X, y)
 
 
 # Predicting a new result
-y_pred = regressor.predict(6.5)
+y_pred = regressor.predict(sc_X.transform(np.array([[6.5]])))
 
 # Visualising the SVR results
 plt.scatter(X, y, color = 'red')
