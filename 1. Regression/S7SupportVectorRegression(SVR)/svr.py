ce65e6c054c4bf620dfe56e18b6e1801a20b26dd
Brain Tucker <brian.briantucker@gmail.com>
2018-01-08 09:56:34 -0600

    svr prediction is a straight line, implementing feature scaling to get actual prediction

--- 1. Regression/S7SupportVectorRegression(SVR)/svr.py
+++ 1. Regression/S7SupportVectorRegression(SVR)/svr.py
@@ -1,3 +1,5 @@
+# Author @Brian Tucker
+# Jan 2018
 # SVR
 
 # Importing the libraries
@@ -13,3 +15,27 @@ y = dataset.iloc[:, 2].values
 # Splitting the dataset into the Training set and Test set
 """from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y=np.ravel(sc_y.fit_transform(y.reshape(-1,1)))
+
+# Fitting the SVR to the dataset
+from sklearn.svm import SVR
+regressor = SVR(kernel='rbf')
+regressor.fit(X, y)
+
+
+# Predicting a new result
+y_pred = regressor.predict(6.5)
+
+# Visualising the SVR results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, regressor.predict(X), color = 'blue')
+plt.title('Truthful or Lying (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
