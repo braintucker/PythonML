e26e1d74480402498c29a7e91479cd1d377ecd11
Brain Tucker <brian.briantucker@gmail.com>
2018-01-09 11:58:27 -0600

    fit logistic regression model to the training set

--- 2. Classification/S12LogisticRegression/logistic_regression.py
+++ 2. Classification/S12LogisticRegression/logistic_regression.py
@@ -2,23 +2,28 @@
 # Logistic Regression
 # January 2018
 
-# Importing the libraries
+# importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
+# importing the dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, [2,3]].values
 Y = dataset.iloc[:, 4].values
 
 
-# Splitting the dataset into the Training set and Test set
+# splitting the dataset into the training set and test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.25, random_state = 0)
 
-# Feature Scaling
+# feature scaling
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
+
+# fitting logistic regression to the training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state=0)
+classifier.fit(X_train, y_train)
