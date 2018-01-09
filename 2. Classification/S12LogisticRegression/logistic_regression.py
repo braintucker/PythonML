02c5353dd3c4be1f4ddfc3e604638a667e827cc5
Brain Tucker <brian.briantucker@gmail.com>
2018-01-09 11:51:45 -0600

    doing data preprocessing and feature scaling

--- 2. Classification/S12LogisticRegression/logistic_regression.py
+++ 2. Classification/S12LogisticRegression/logistic_regression.py
@@ -1,8 +1,24 @@
 # Author @Brian Tucker
-# January 2018
 # Logistic Regression
+# January 2018
 
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2,3]].values
+Y = dataset.iloc[:, 4].values
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
