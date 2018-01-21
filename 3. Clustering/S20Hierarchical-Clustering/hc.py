9febad6cb024d46955886def55e0d5f942e694c4
Brain Tucker <brian.briantucker@gmail.com>
2018-01-21 10:12:51 -0600

    plotting the dendrogram to find the optimal # of clusters

--- 3. Clustering/S20Hierarchical-Clustering/hc.py
+++ 3. Clustering/S20Hierarchical-Clustering/hc.py
@@ -1,1 +1,23 @@
 # Hierarchical Clustering
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# importing the mall dataset with pandas
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:, [3, 4]].values
+# y = dataset.iloc[:, 3].values
+
+# using the dendrogram to find the optimal number of clusters
+import scipy.cluster.hierarchy as sch
+dendrogram = sch.dendrogram(sch.linkage(X, method='ward'))
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean distances')
+plt.show()
+
+
+
+
