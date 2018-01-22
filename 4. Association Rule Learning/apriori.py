d454a26305142643a4defa6a72b00e962e2e7dc9
Brain Tucker <brian.briantucker@gmail.com>
2018-01-22 12:23:11 -0600

    training the apriori on the dataset

--- 4. Association Rule Learning/apriori.py
+++ 4. Association Rule Learning/apriori.py
@@ -1,6 +1,6 @@
 # Apriori
 
-# Importing the libraries
+# importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -10,3 +10,7 @@ dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
 transactions = []
 for i in range(0, 7501):
     transactions.append([str(dataset.values[i, j]) for j in range(0, 20)])
+
+# training the apriori on the dataset
+from apyori import apriori as ap
+rules = ap(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = , min_length = 2)
